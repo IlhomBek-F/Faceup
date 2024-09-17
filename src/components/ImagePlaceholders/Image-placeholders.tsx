6f3272b652cef34ae1492cb13080542348545868
Ilhomjon <futurewithme1221@gmail.com>
2024-09-17 14:56:22 +0500

    feat: Implemented image placeholde

--- src/components/ImagePlaceholders/Image-placeholders.tsx
+++ src/components/ImagePlaceholders/Image-placeholders.tsx
@@ -1,10 +1,24 @@
+import { Skeleton } from 'antd'
 import './image-placeholder.css'
 
 function ImagePlaceholders() {
 
     return (
-        <div className="placeholder">
-          <p>Loading....</p>
+        <div className="image-container">
+           <div className='image-grid'>
+               <div className='image-column-1'>
+               <Skeleton.Image active style={{width: '400px', height: '400px'}}/>
+               <Skeleton.Image active style={{width: '400px', height: '400px'}}/>
+               </div>
+               <div className='image-column-2'>
+               <Skeleton.Image active style={{width: '400px', height: '300px'}}/>
+               <Skeleton.Image active style={{width: '400px', height: '500px'}}/>
+               </div>
+               <div className='image-column-3'>
+               <Skeleton.Image active style={{width: '400px', height: '500px'}}/>
+               <Skeleton.Image active style={{width: '400px', height: '300px'}}/>
+               </div>
+           </div>
         </div>
     )
 }
