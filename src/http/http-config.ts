ba3f9b75f48b8c3636569a8b6f3d3f0f50285e95
Ilhomjon <futurewithme1221@gmail.com>
2024-09-19 15:03:18 +0500

    fix: fixed showing server error message

--- src/http/http-config.ts
+++ src/http/http-config.ts
@@ -15,7 +15,7 @@ export const http = axios.create({
 http.interceptors.request.use(
     (config) => config,
     error => {
-        Promise.reject(error.toJSON() || error.message);
+        Promise.reject(error || error.message);
     }
 );
 
@@ -27,6 +27,8 @@ http.interceptors.response.use(({data}) => {
     
     return normalizeResponseData(resImagesLength, images, data.total)as unknown as AxiosResponse<any, any>
 }, error => {
-    return Promise.reject(error.toJSON())
+    const backendErrorMessage = error.response.data.errors.join(' ') || 'An unknown error occurred';
+    // Optionally, reject with a more specific error message
+    return Promise.reject(new Error(backendErrorMessage));
 })
 
