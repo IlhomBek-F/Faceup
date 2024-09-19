24f09fed00980d1c03faa6264b7dd9434348fe1f
Ilhomjon <futurewithme1221@gmail.com>
2024-09-19 11:11:21 +0500

    design: added like and expand icon

--- src/components/ImageItem/Image-item.tsx
+++ src/components/ImageItem/Image-item.tsx
@@ -1,11 +1,18 @@
-import { ExpandOutlined } from '@ant-design/icons';
+import { ExpandOutlined, LikeOutlined } from '@ant-design/icons';
 import './image-item.css';
+import { Flex } from 'antd';
 
 function ImageItem({image}) {
     return (
-        <div style={{position: 'relative'}}>                               
+        <div className='image-holder' >                               
                 <ExpandOutlined className='expand-icon'/>
                 <img src={image.urls.small} alt='image'  className='img' />
+                <footer className='image-footer'>
+                    <Flex align='center' gap='8px'>
+                      <LikeOutlined className='like-icon' size={50}/>
+                      <p className='like-count'>{image.likes}</p>
+                    </Flex>
+                </footer>
         </div>
     )
 }
