91e70e817a8454ef2ad6744455b03259a89b254b
Ilhomjon <futurewithme1221@gmail.com>
2024-09-20 15:31:48 +0500

    feat: Added blur placeholder for image and installed react-blurhash library

--- src/components/ImageItem/Image-item.tsx
+++ src/components/ImageItem/Image-item.tsx
@@ -4,12 +4,14 @@ import { ShareImage } from '../ShareImage/Share-image';
 import { useCallback, useState } from 'react';
 import { getDownloadImageUrl } from '../../service';
 import { downloadImage } from '../../helper';
-import './image-item.css';
 import { DownloadSpin } from '../DownloadSpin/Download-spin';
+import { Blurhash } from 'react-blurhash';
+import './image-item.css';
 
 function ImageItem({image}) {
-    const {alt_description, urls, links, likes, user} = image;
+    const {alt_description, urls, links, likes, user, blur_hash} = image;
     const [downloading, setDownloading] = useState(false);
+    const [completed, setCompleted] = useState(false);
 
     const handleDownloadImage = useCallback(() => {
       setDownloading(true);
@@ -23,23 +25,34 @@ function ImageItem({image}) {
 
     return (
         <div className='image-holder'>
-                 {downloading && <DownloadSpin />}    
-                 <Avatar src={user.profile_image.medium} icon={<UserOutlined />} className='avatar'/>
-                 <aside className='image-side'>
-                    <Flex vertical gap='10px'>
-                      <ShareImage shareUrl={urls.small}/>
-                      <DownloadOutlined className='download-icon' onClick={handleDownloadImage}/>    
+                 {downloading && <DownloadSpin />}  
+                 {completed && <>
+                  <Avatar src={user.profile_image.medium} icon={<UserOutlined />} className='avatar'/>
+                    <aside className='image-side'>
+                     <Flex vertical gap='10px'>
+                       <ShareImage shareUrl={urls.small}/>
+                       <DownloadOutlined className='download-icon' onClick={handleDownloadImage}/>    
                     </Flex>
-                </aside>        
+                    </aside>
+                 </>}
                 <Image.PreviewGroup>
-                  <Image src={urls.small} alt={alt_description}/>
-                </Image.PreviewGroup>      
-                <footer className='image-footer'>
+                  <Image preview={completed} src={urls.small} alt={alt_description}  placeholder={
+                    <Blurhash
+                   hash={blur_hash}
+                   resolutionX={32}
+                   resolutionY={32}
+                   width='100%'
+                   height='100%'
+                   punch={1}
+                 />    
+                  } onLoad={() => setCompleted(true)}/>
+                </Image.PreviewGroup>
+                {completed &&  <footer className='image-footer'>
                     <Flex align='center' gap='8px'>
                       <LikeOutlined className='like-icon' size={50}/>
                       <p className='like-count'>{likes}</p>
                     </Flex>
-                </footer>
+                </footer>}     
         </div>
     )
 }
