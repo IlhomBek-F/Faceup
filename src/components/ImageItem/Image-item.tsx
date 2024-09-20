78ac2e6221f133d69b5e2f9f48fdc286fcbc3f6b
Ilhomjon <futurewithme1221@gmail.com>
2024-09-20 09:46:59 +0500

    design: Created a user avatar for every image

--- src/components/ImageItem/Image-item.tsx
+++ src/components/ImageItem/Image-item.tsx
@@ -1,14 +1,16 @@
-import { DownloadOutlined, LikeOutlined } from '@ant-design/icons';
-import { Flex , Image} from 'antd';
+import { DownloadOutlined, LikeOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Flex , Image} from 'antd';
 import { ShareImage } from '../ShareImage/Share-image';
 import './image-item.css';
 
 function ImageItem({image, handleDownload}) {
-    const {alt_description, urls, links, likes} = image;
+    const {alt_description, urls, links, likes, user} = image;
    
     return (
         <div className='image-holder'>    
+                 <Avatar src={user.profile_image.medium} icon={<UserOutlined />} className='avatar'/>
                  <aside className='image-side'>
+                   
                     <Flex vertical gap='10px'>
                       <ShareImage shareUrl={urls.small}/>
                       <DownloadOutlined className='download-icon' onClick={() => handleDownload(links.download_location, alt_description)}/>    
