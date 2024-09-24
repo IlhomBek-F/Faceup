09476b431c54ca87554137d8467b2d28f1977edd
Ilhomjon <futurewithme1221@gmail.com>
2024-09-24 13:04:17 +0500

    fixed: design issue

--- src/components/ImageItem/Image-item.tsx
+++ src/components/ImageItem/Image-item.tsx
@@ -1,13 +1,13 @@
 import { DownloadOutlined, LikeOutlined, UserOutlined } from '@ant-design/icons';
 import { Avatar, Flex, Image } from 'antd';
 import { ShareImage } from '../ShareImage/Share-image';
-import { SyntheticEvent, useCallback, useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { getDownloadImageUrl } from '../../service';
 import { downloadImage } from '@/helper';
 import { DownloadSpin } from '../DownloadSpin/Download-spin';
 import { Blurhash } from 'react-blurhash';
-import './image-item.css';
 import React from 'react';
+import './image-item.css';
 
 function ImageItem({ image }) {
   const { alt_description, urls, links, likes, user, blur_hash } = image;
@@ -37,7 +37,7 @@ function ImageItem({ image }) {
         </aside>
       </>}
       <Image.PreviewGroup>
-        <Image preview={completed} src={urls.small} alt={alt_description} className='img' placeholder={
+        <Image preview={completed} src={urls.small} alt={alt_description} placeholder={
           <Blurhash
             hash={blur_hash}
             resolutionX={32}
